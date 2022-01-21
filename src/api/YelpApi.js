434c54ab2b76306a1faf3b13e0d9813155caa114
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2022-01-20 22:20:10 -0800

    m

--- src/api/YelpApi.js
+++ src/api/YelpApi.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 
 export default function getYelpData(queryParams) {
     const query = queryString.stringify(queryParams)       
-    return fetch(`${process.env.REACT_APP_YELP_API}?${query}`);  
+    return fetch(`${process.env.REACT_APP_BASE_URL}/api?${query}`);  
    console.debug("API CALL:", query)
 }
 
